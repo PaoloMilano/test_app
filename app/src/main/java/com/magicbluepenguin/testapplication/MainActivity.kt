d433eebf25e497a7963999146e2bd01aa0850170
Paolo Milano <spacitron999@gmail.com>
2019-09-02 22:38:14 +0200

    Created application with basic dependency structure

---
+++ app/src/main/java/com/magicbluepenguin/testapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.magicbluepenguin.testapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.magicbluepenguin.testapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
